8afc14bf48ef2435b1a748f376257615c61ac98f
André Rangel <andreavila.rangel@gmail.com>
2025-12-08 21:18:21 -0300

    feat: google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,20 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth overflow-x-hidden">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5D39P7HS');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
@@ -104,6 +118,17 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} font-primary antialiased overflow-x-hidden max-w-[100vw]`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5D39P7HS"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
       </body>
     </html>
